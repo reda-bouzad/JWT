fcaa7d6280f786510b0043c1993b7acc667c03cf
reda-bouzad <redabouzadfst@gmail.com>
2023-09-08 16:02:40 +0100

    v1

--- src/main/java/maroc/reda/jwt/configuration/Cors.java
+++ src/main/java/maroc/reda/jwt/configuration/Cors.java
@@ -4,24 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class Cors {
+    @Configuration
+    public class Cors {
 
-    private static final String GET = "GET";
-    private static final String POST = "POST";
-    private static final String DELETE = "DELETE";
-    private static final String PUT = "PUT";
+        private static final String GET = "GET";
+        private static final String POST = "POST";
+        private static final String DELETE = "DELETE";
+        private static final String PUT = "PUT";
 
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods(GET, POST, DELETE, PUT)
-                        .allowedHeaders("*")
-                        .allowedOriginPatterns("*")
-                        .allowCredentials(true);
-            }
-        };
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods(GET, POST, DELETE, PUT)
+                            .allowedHeaders("*")
+                            .allowedOriginPatterns("*")
+                            .allowCredentials(true);
+                }
+            };
+        }
     }
-}
